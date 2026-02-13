dabde19e7329da36f131d8ff8a162094258343e9
jzaruma1209 <jzaruma1209@gmail.com>
2026-02-13 15:03:18 -0500

    advances

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       {/* Contenido centrado */}
-      <div className="flex flex-1 flex-col items-center justify-center px-4">
+      <div className="flex flex-1 flex-col items-center justify-center px-4 -mt-[20vh] sm:mt-0">
         {/* Video con overlay play solo en móvil */}
         <div className="relative w-full max-w-2xl">
           {/* Video real (siempre visible en desktop, o después de tocar play en móvil) */}
